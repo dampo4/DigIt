1f8318cf6e1754c9068152a9b52033161bca5b1e
dampo4 <33032031+dampo4@users.noreply.github.com>
2021-02-20 14:50:26 +0000

    Digging code added

---
+++ New Unity Project/Assets/Dig.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class Dig : MonoBehaviour
+{
+    private InputDevice device;
+    private bool dig;
+    Collider collision;
+    // Start is called before the first frame update
+    void Start()
+    {
+        device = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        device.TryGetFeatureValue(CommonUsages.primaryButton, out dig);
+        if (dig == true && collision.gameObject.tag == "Artefact")
+        {
+            Debug.Log("Digging");
+        }
+    }
+    void OnTriggerEnter(Collider collision)
+    {
+        this.collision = collision;
+        Debug.Log(collision.gameObject.tag);
+    }
+
+    void OnTriggerExit(Collider collision)
+    {
+        this.collision = null;
+    }
+}
